e7358946b1002f345d17204bb1b7df5f7476101c
Leul Tesfaye <ltesfaye31@gmail.com>
2018-04-18 14:38:43 -0400

    Working Animation Timeline

--- a6/src/anim/AnimTimeline.java
+++ a6/src/anim/AnimTimeline.java
@@ -2,6 +2,8 @@ package anim;
 
 import java.util.TreeSet;
 
+import org.lwjgl.util.vector.Matrix;
+
 import common.SceneObject;
 import egl.math.Matrix3;
 import egl.math.Matrix4;
@@ -51,6 +53,12 @@ public class AnimTimeline {
 	public void addKeyFrame(int frame, Matrix4 t) {
 		// TODO#A6: Add an AnimKeyframe to frames and set its transformation
 		
+		// creates the animation frame 
+		AnimKeyframe f = new AnimKeyframe(frame); 
+		f.transformation.set(t);
+		
+		// adds the animation frame
+		frames.add(f);
 	}
 	/**
 	 * Remove a keyframe from the timeline.  If the timeline is empty,
@@ -63,9 +71,20 @@ public class AnimTimeline {
 		// TODO#A6: Delete a frame, you might want to use Treeset.remove
 		// If there is no frame after deletion, add back this frame.
 		
+		// we create a local copy of this frame we want to remove
+		AnimKeyframe f = new AnimKeyframe(frame);
+		f.transformation.set(t);
+		
+		// we remove it 
+		frames.remove(f);
+		
+		if (frames.isEmpty()) {
+			// if there is no frame in the current animation we add this back 
+			frames.add(f);
+		}
+		
 	}
 
-	
 	/**
 	 * Takes a rotation matrix and decomposes into Euler angles. 
 	 * Returns a Vector3 containing the X, Y, and Z degrees in radians.
@@ -99,6 +118,96 @@ public class AnimTimeline {
 		// 1 - Linear interpolation of quaternions,
 		// 2 - Spherical linear interpolation of quaternions.
 		
+		AnimKeyframe f = new AnimKeyframe(curFrame);
+		AnimKeyframe prev = frames.floor(f);
+		AnimKeyframe next= frames.ceiling(f);
 		
+	if (frames.size() >1 && prev != null && next != null && !next.equals(prev)) {
+		
+			
+			
+			// The current interpolation
+			float t = (next.frame -curFrame);
+			t /= (next.frame - prev.frame);
+		
+			// gets the translation of both frames
+			Vector3 trans0 =prev.transformation.getTrans();
+			Vector3 trans1 = next.transformation.getTrans();
+			
+			Vector3 transout = trans0.mul(t).add(trans1.mul(1-t));
+			
+			Matrix3 rot_s0 = prev.transformation.getAxes();
+			Matrix3 rot_s1 = next.transformation.getAxes();
+			
+			//creates scale and rotation matricies 
+			Matrix3 scale0  = new Matrix3();
+			Matrix3 scale1 = new Matrix3();
+			Matrix3 scaleout = new Matrix3();
+			
+			Matrix3 rot0  = new Matrix3();
+			Matrix3 rot1 = new Matrix3();
+			
+			
+			rot_s0.polar_decomp(rot0, scale0);
+			rot_s1.polar_decomp(rot1, scale1);
+			
+			// assigns the liner interpolation for scale
+			scaleout.interpolate(scale0, scale1, 1-t);
+	
+			
+			Quat quat0 = new Quat(rot0);
+			Quat quat1 = new Quat(rot1);
+			
+			Quat out = new Quat();
+			Matrix3 rotout = new Matrix3();
+			Vector3 lin_angle = new Vector3(); //used in case  of linear interpolation;
+			switch (rotation) {
+			
+ 		
+				case 0:
+						Vector3 temp0 = eulerDecomp(rot0);
+						Vector3 temp1 = eulerDecomp(rot1);
+					     lin_angle = temp0.mul(t).add(temp1.mul(1-t));
+						break;
+				case 1:
+						 out = quat0.scale(t).add(quat1.scale(1-t));
+						 out.normalize();
+						 
+						break;
+				case 2:
+					
+						out = Quat.slerp(quat0, quat1, 1-t);
+						break;
+					
+				
+				default:break;
+			}
+			
+			if (rotation == 0) {
+				Matrix3 rotx = Matrix3.createRotationX(lin_angle.x);
+				Matrix3 roty = Matrix3.createRotationY(lin_angle.y);
+				Matrix3 rotz = Matrix3.createRotationZ(lin_angle.z);
+				
+				rotout = rotz.mulBefore(roty.mulBefore(rotx));
+			}
+			else {
+				out.toRotationMatrix(rotout);
+			}
+			Matrix4 everything = new Matrix4(rotout.mulBefore(scaleout));
+			
+			everything.set(0, 3, transout.x);
+			everything.set(1, 3, transout.y);
+			everything.set(2, 3, transout.z);
+			everything.set(3, 3, 1);
+			object.transformation.set(everything);
+			
+		}
+	
+	else {
+		//handles edge cases
+			AnimKeyframe val = next !=null ? next : prev;
+			
+			object.transformation.set(val.transformation);
+		}
 	}
 }
